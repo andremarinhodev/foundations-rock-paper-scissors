c53b9f2d734bcb9c093bcf8cf5464cb801d7c1be
andremarinhodev <79175971+andremarinhodev@users.noreply.github.com>
2023-09-20 01:55:22 -0300

    Add computer choice function

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3 ) + 1;
+    switch (choice){
+        case 1:
+            return 'Rock';
+        case 2:
+            return 'Paper';
+        case 3:
+            return 'Scissors';
+    }
+}
+
+console.log(getComputerChoice());
